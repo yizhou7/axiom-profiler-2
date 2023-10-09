94013d841571a4053aafc6686f4224134d811b10
ojyrkinen <oskari.jyrkinen@gmail.com>
2023-10-09 10:43:49 +0200

    Removed test comment

--- axiom-profiler-GUI/src/main.rs
+++ axiom-profiler-GUI/src/main.rs
@@ -30,7 +30,6 @@ impl Component for FileDataComponent {
         }
     }
 
-    // this is a test comment
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             Msg::Files(files) => {
