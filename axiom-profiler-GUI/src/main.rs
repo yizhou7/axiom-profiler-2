4f7e8efa794033ad385beb507b1c159be23a20f5
ojyrkinen <oskari.jyrkinen@gmail.com>
2023-10-10 09:31:49 +0200

    Seperated code into two modules

--- axiom-profiler-GUI/src/main.rs
+++ axiom-profiler-GUI/src/main.rs
@@ -1,12 +1,10 @@
 use gloo_file::{callbacks::FileReader, File};
-use scraper::{self, Selector};
 use std::collections::HashMap;
 use std::str::from_utf8;
 use web_sys::{Event, HtmlInputElement};
 use yew::prelude::*;
 use yew_router::prelude::*;
-use prototype::parsers::{z3parser1, LogParser};
-use viz_js::VizInstance;
+use crate::svg_graph::*;
 
 mod svg_graph;
 pub enum Msg {
@@ -101,102 +99,6 @@ impl FileDataComponent {
             <SVGResult trace_file_text={AttrValue::from(data.to_string())}/>
             </div>
         }
-    }
-}
-
-#[derive(Properties, PartialEq)]
-pub struct SVGProps {
-    pub trace_file_text: AttrValue,
-}
-
-#[function_component(SVGResult)]
-fn svg_result(props: &SVGProps) -> Html {
-    log::debug!("SVG result");
-    let svg_text = use_state(|| html! {});
-    let g_selector = Selector::parse("g > g").unwrap();
-    let svg_selector = Selector::parse("svg").unwrap();
-    let onclick = {
-        let text = props.trace_file_text.to_string();
-        let svg_text = svg_text.clone();
-        let g_selector = g_selector.clone();
-        let svg_selector = svg_selector.clone();
-        Callback::from(move |_| {
-            let text = text.to_string();
-            let svg_text = svg_text.clone();
-            let g_selector = g_selector.clone();
-            let svg_selector = svg_selector.clone();
-            log::debug!("use effect");
-            wasm_bindgen_futures::spawn_local(
-                async move {
-                    let mut parser = z3parser1::Z3Parser1::new();
-                    parser.process_log(text);
-                    let dot_output = parser.get_dot_output_as_string();
-                    let graphviz = VizInstance::new().await;
-                    let svg = graphviz
-                        .render_svg_element(dot_output, viz_js::Options::default())
-                        .expect("Could not render graphviz");
-                    let fetched_svg = svg.outer_html(); 
-                                        // let fetched_svg: String = crate::svg_graph::create_svg_graph(&parser.get_sorted_dependencies());  
-                    log::debug!("request done");
-                    let length = fetched_svg.len();
-                    log::debug!("new svg: {} bytes", length);
-                    let svg = scraper::Html::parse_document(&fetched_svg);
-                    let g_tags =
-                        scraper::Html::select(&svg, &g_selector);
-                    log::debug!("selected");
-                    let _nodes = g_tags
-                        .map(|node| {
-                                let inner_html: AttrValue = 
-                                node.inner_html().into();
-                                let id: AttrValue = node.value().id().unwrap().to_string().into();
-                                let class = classes!(node.value().classes().map(String::from).collect::<Vec<String>>());
-                            html! {
-                            <Node inner_html={inner_html} id={id} class={class}/>
-                            }
-                        });
-                    let mut svg_tag = scraper::Html::select(&svg, &svg_selector);
-                    let node = svg_tag.next().unwrap().value();
-                    let _width = node.attr("width").unwrap().to_string();
-                    let _height = node.attr("height").unwrap().to_string();
-                    let _view_box = node.attr("viewBox").unwrap().to_string();
-                    log::debug!("made nodes");
-                    let svg_result = AttrValue::from(fetched_svg);
-                    svg_text.set(Html::from_html_unchecked(svg_result));
-                    log::debug!("set state");
-                },
-                   
-            );
-        })
-    };
-
-    html! {
-        <>
-        <button onclick={onclick}>{"Load file"}</button>
-        <br/>
-        // <textarea rows="50" cols="100" />
-        {(*svg_text).clone()}
-        </>
-    }
-}
-
-#[derive(Properties, PartialEq)]
-pub struct NodeProps {
-    pub inner_html: AttrValue,
-    pub id: AttrValue,
-    pub class: Classes,
-}
-
-#[function_component(Node)]
-fn node(props: &NodeProps) -> Html {
-    let state = use_state(|| props.id.clone());
-    let onclick = move |_| {
-        log::debug!("{}", *state);
-    };
-    let inner = Html::from_html_unchecked(props.inner_html.clone().into());
-    html! {
-        <g id={props.id.clone()} class={props.class.clone()} onclick={onclick.clone()}>
-        {inner}
-        </g>
     }
 }
 
