b980c4c17f9daa6cc29fd66d3bded288e0986414
ojyrkinen <oskari.jyrkinen@gmail.com>
2023-10-10 14:39:39 +0200

    Revert "Buggy commit"
    This reverts commit 60d812f2b28444710915a92c8b43e910ab76eb89.

--- axiom-profiler-GUI/src/svg_graph.rs
+++ axiom-profiler-GUI/src/svg_graph.rs
@@ -8,19 +8,10 @@ pub struct SVGProps {
     pub trace_file_text: AttrValue,
 }
 
-#[derive(Default,Clone)]
-struct SVGProperties {
-    width: String,
-    height: String,
-    view_box: String,
-    // xmlns: String,
-    // xmlns_xlink: String,
-}
-
 #[function_component(SVGResult)]
 pub fn svg_result(props: &SVGProps) -> Html {
     log::debug!("SVG result");
-    let svg_text = use_state(|| (html! {}, Vec::new(), SVGProperties::default()));
+    let svg_text = use_state(|| (html! {}, Vec::new()));
     let g_selector = Selector::parse("g > g").unwrap();
     let svg_selector = Selector::parse("svg").unwrap();
     let onclick = {
@@ -48,8 +39,6 @@ pub fn svg_result(props: &SVGProps) -> Html {
                     let length = fetched_svg.len();
                     log::debug!("new svg: {} bytes", length);
                     let svg = scraper::Html::parse_document(&fetched_svg);
-                    let parsed = Html::from_html_unchecked(AttrValue::from(fetched_svg));
-                    let parsed_vlist = parsed.to_vlist_mut();
                     let g_tags =
                         scraper::Html::select(&svg, &g_selector);
                     log::debug!("selected");
@@ -69,21 +58,9 @@ pub fn svg_result(props: &SVGProps) -> Html {
                     let _width = node.attr("width").unwrap().to_string();
                     let _height = node.attr("height").unwrap().to_string();
                     let _view_box = node.attr("viewBox").unwrap().to_string();
-                    // let _xmlns = node.attr("xmlns").unwrap().to_string();
-                    // let _xmlns_xlink = node.attr("xmlns:xlink").unwrap().to_string();
-                    let svg_properties = SVGProperties {
-                        width: _width,
-                        height: _height,
-                        view_box: _view_box,
-                        // xmlns: _xmlns,
-                        // xmlns_xlink: _xmlns_xlink,
-                    }; 
                     log::debug!("made nodes");
                     let svg_result = AttrValue::from(fetched_svg);
-                    svg_text.set((
-                        Html::from_html_unchecked(svg_result),
-                        _nodes,
-                        svg_properties));
+                    svg_text.set((Html::from_html_unchecked(svg_result),_nodes));
                     log::debug!("set state");
                 },
                    
@@ -91,20 +68,13 @@ pub fn svg_result(props: &SVGProps) -> Html {
         })
     };
 
-    let svg_properties = (*svg_text).2.clone();
     html! {
         <>
         <button onclick={onclick}>{"Load file"}</button>
         <br/>
         // <textarea rows="50" cols="100" />
         {(*svg_text).0.clone()}
-        // <svg width={svg_properties.width} height={svg_properties.height} viewBox={svg_properties.view_box} xmlns="http://www.w3.org/2000/svg">
-        <svg width="206" height="116" viewBox="0 0 206 116" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink">
-            <g id="graph0" class="graph" transform="scale(1 1) rotate(0) translate(4 112)">
-            <polygon fill="white" stroke="none" points="-4,4 -4,-112 202,-112 202,4 -4,4"></polygon>
-            { for (*svg_text).1.clone()}
-            </g>
-        </svg>
+        { for (*svg_text).1.clone()}
         </>
     }
 }
