96244123815fda862f0ed10144a2f79935cd6d89
ojyrkinen <oskari.jyrkinen@gmail.com>
2023-10-15 09:53:29 +0200

    No longer using from_html_unchecked

--- axiom-profiler-GUI/src/svg_graph.rs
+++ axiom-profiler-GUI/src/svg_graph.rs
@@ -12,27 +12,18 @@ pub struct SVGProps {
 #[function_component(SVGResult)]
 pub fn svg_result(props: &SVGProps) -> Html {
     log::debug!("SVG result");
-    // let svg_text = use_state(|| (html! {}, Vec::new()));
     let svg_text = use_state(|| String::new());
-    // let svg_text = use_state(|| html! {});
-    let g_selector = Selector::parse("g > g").unwrap();
-    let svg_selector = Selector::parse("svg").unwrap();
     let onclick = {
         let text = props.trace_file_text.to_string();
         let svg_text = svg_text.clone();
-        let g_selector = g_selector.clone();
-        let svg_selector = svg_selector.clone();
         Callback::from(move |_| {
             let text = text.to_string();
             let svg_text = svg_text.clone();
-            let g_selector = g_selector.clone();
-            let svg_selector = svg_selector.clone();
             log::debug!("use effect");
             wasm_bindgen_futures::spawn_local(
                 async move {
                     let mut parser = z3parser1::Z3Parser1::new();
                     parser.process_log(text);
-                    // let dot_output = parser.get_dot_output_as_string();
                     let qi_graph = parser.get_instantiation_graph();
                     let dot_output = format!("{:?}", Dot::with_config(qi_graph, &[Config::EdgeNoLabel])); 
                     let graphviz = VizInstance::new().await;
@@ -40,58 +31,21 @@ pub fn svg_result(props: &SVGProps) -> Html {
                         .render_svg_element(dot_output, viz_js::Options::default())
                         .expect("Could not render graphviz");
                     let fetched_svg = svg.outer_html(); 
-                    // log::debug!("request done");
-                    // let length = fetched_svg.len();
-                    // log::debug!("new svg: {} bytes", length);
-                    // let svg = scraper::Html::parse_document(&fetched_svg);
-                    // let g_tags =
-                    //     scraper::Html::select(&svg, &g_selector);
-                    // log::debug!("selected");
-                    // let g_nodes_html: Vec<AttrValue> = g_tags
-                    //     .map(|node| {
-                    //             let inner_html: AttrValue = 
-                    //             node.inner_html().into();
-                    //             inner_html
-                            //     let id_str = node.value().id().unwrap().to_string();
-                            //     let id : AttrValue = id_str.clone().into();
-                            //     let class = classes!(node.value().classes().map(String::from).collect::<Vec<String>>());
-                            // html! {
-                            //     {if id_str.contains("a_") {
-                            //         html! {}
-                            //     } else {
-                            //         html! { <Node inner_html={inner_html} id={id} class={class}/> }
-                            //     }}
-                            // }
-                        // }).collect();
-                    // let mut svg_tag = scraper::Html::select(&svg, &svg_selector);
-                    // let node = svg_tag.next().unwrap().value();
-                    // let _width = node.attr("width").unwrap().to_string();
-                    // let _height = node.attr("height").unwrap().to_string();
-                    // let _view_box = node.attr("viewBox").unwrap().to_string();
-                    // log::debug!("made nodes");
-                    // let svg_result = AttrValue::from(fetched_svg);
                     svg_text.set(fetched_svg);
-                    // log::debug!("set state");
-                    // let svg_result = AttrValue::from(fetched_svg);
-                    // svg_text.set(Html::from_html_unchecked(svg_result));
                 },
                    
             );
         })
     };
-
     html! {
         <>
             <div>
             <button onclick={onclick}>{"Load file"}</button>
             </div>
-            // {(*svg_text).clone()}
             <svg xmlns="http://www.w3.org/2000/svg" width="206pt" height="116pt" viewBox="0.00 0.00 206.00 116.00">
                 <g id="graph0" class="graph" transform="scale (1 1) rotate(0) translate(4 112)">
                     <polygon fill="white" stroke="none" points="-4,4 -4,-112 202,-112 202,4 -4,4"></polygon>
                     <NodeList svg_text={(*svg_text).clone()} />
-                    // { for g_nodes }
-                    // { for (*svg_text).clone().1 }
                 </g>
             </svg>
         </>
@@ -109,14 +63,22 @@ fn node_list(NodeListProps { svg_text }: &NodeListProps) -> Html {
     let svg = scraper::Html::parse_document(svg_text);
     let g_tags = scraper::Html::select(&svg, &g_selector);
     g_tags.map(|node| {
-        let inner_html: AttrValue = 
-        node.inner_html().into();
-        let id: AttrValue = node.value().id().unwrap().to_string().into();
-        let class = classes!(node.value().classes().map(String::from).collect::<Vec<String>>());
-        let inner_html = Html::from_html_unchecked(inner_html);
+        let html: AttrValue = node.html().into();
+        // let inner_html: AttrValue = 
+        // node.inner_html().into();
+        // let id: AttrValue = node.value().id().unwrap().to_string().into();
+        // let class = classes!(node.value().classes().map(String::from).collect::<Vec<String>>());
+        // let inner_html = Html::from_html_unchecked(inner_html);
+        let outer_html = Html::from_html_unchecked(html);
         html! {
         // <Node inner_html={inner_html} id={id} class={class}/>
-        <g id={id} class={class}>{inner_html}</g>
+        // <g id={id} class={class}>{inner_html}</g>
+        // outer_html 
+        <g id="node1" class="node">
+            <title>{0}</title>
+            <ellipse stroke="black" ry="18" fill="none" cy="-90" cx="27" rx="27"></ellipse>
+            <text y="-85.8" x="27" font-size="14.00" text-anchor="middle" font-family="Times,serif">{722}</text>
+        </g>
         }
     }).collect()
 }
