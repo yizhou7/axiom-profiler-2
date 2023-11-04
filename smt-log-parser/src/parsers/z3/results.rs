5dac658cfa2d998130ae5332fe5d56d88fa23ef9
ojyrkinen <oskari.jyrkinen@gmail.com>
2023-11-04 11:45:45 +0100

    Functional code for constructing QI-graph

--- smt-log-parser/src/parsers/z3/results.rs
+++ smt-log-parser/src/parsers/z3/results.rs
@@ -2,35 +2,36 @@ use std::collections::BTreeMap;
 
 use petgraph::Graph;
 use petgraph::graph::NodeIndex;
+use gloo_console::log;
 
 use super::z3parser::Z3Parser;
-use crate::items::*;
 
 impl Z3Parser {
     pub fn get_instantiation_graph(&self) -> (petgraph::Graph<usize, ()>, BTreeMap<usize, usize>) {
         let mut qi_graph = Graph::<usize, ()>::new();
-        let is_theory_inst = |d: &Dependency| {
-            d.quant_discovered
-        }; 
-        let fresh_line_nr = |qi_graph: &Graph::<usize, ()>, line_nr: usize| qi_graph.node_weights().all(|&line| line != line_nr);
-        let mut node_of_line_nr: BTreeMap<usize, NodeIndex> = BTreeMap::new(); 
         let mut line_nr_of_node: BTreeMap<usize, usize> = BTreeMap::new(); 
-        for dep in &self.dependencies {
-            if !is_theory_inst(dep) { 
-                if let (from, Some(to)) = (dep.from, dep.to) {
-                    if fresh_line_nr(&qi_graph, to) {
-                        let to_node = qi_graph.add_node(to);
-                        node_of_line_nr.insert(to, to_node);
-                        line_nr_of_node.insert(to_node.index(), to);
-                    }
-                    if from > 0 {
-                        if fresh_line_nr(&qi_graph, from) {
-                            let from_node = qi_graph.add_node(from);
-                            node_of_line_nr.insert(from, from_node);
-                            line_nr_of_node.insert(from_node.index(), from);
-                        }
-                        if let (Some(&from_node_idx), Some(&to_node_idx)) = (node_of_line_nr.get(&from), node_of_line_nr.get(&to)) {
-                            qi_graph.add_edge(from_node_idx, to_node_idx, ());
+        let mut node_of_line_nr: BTreeMap<usize, NodeIndex> = BTreeMap::new(); 
+        let fresh_line_nr = |qi_graph: &Graph::<usize, ()>, line_nr: usize| qi_graph.node_weights().all(|&line| line != line_nr);
+        let insts = &self.instantiations;
+        for to_inst in insts.iter().filter(|inst| !inst.quant_discovered) {
+            let from_iidxs = &to_inst.dep_instantiations;
+            if let Some(to) = to_inst.line_no {
+                if fresh_line_nr(&qi_graph, to) {
+                    let to_node = qi_graph.add_node(to);
+                    node_of_line_nr.insert(to, to_node);
+                    line_nr_of_node.insert(to_node.index(), to);
+                }
+                for from_inst in from_iidxs.iter().filter_map(|&iidx| insts.get(iidx)) {
+                    if let Some(from) = from_inst.line_no {
+                        if from > 0 {
+                            if fresh_line_nr(&qi_graph, from) {
+                                let from_node = qi_graph.add_node(from);
+                                node_of_line_nr.insert(from, from_node);
+                                line_nr_of_node.insert(from_node.index(), from);
+                            }
+                            if let (Some(&from_node_idx), Some(&to_node_idx)) = (node_of_line_nr.get(&from), node_of_line_nr.get(&to)) {
+                                qi_graph.add_edge(from_node_idx, to_node_idx, ());
+                            }
                         }
                     }
                 }
@@ -38,4 +39,5 @@ impl Z3Parser {
         }
         (qi_graph, line_nr_of_node)
     }
+
 }
