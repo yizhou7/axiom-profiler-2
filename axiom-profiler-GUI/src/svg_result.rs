2eb076a2cec1f191239df1d4c3e7214645adcdbc
ojyrkinen <oskari.jyrkinen@gmail.com>
2023-11-08 10:03:53 +0100

    Moved parser out of callback

--- axiom-profiler-GUI/src/svg_result.rs
+++ axiom-profiler-GUI/src/svg_result.rs
@@ -21,18 +21,15 @@ pub fn svg_result(props: &SVGProps) -> Html {
     let max_line_nr = use_reducer(InputValue::default);
     let exclude_theory_inst = use_bool_toggle(true);
     let max_instantiations = use_reducer(InputValue::default);
+    let parser = use_state(|| Z3Parser::from_str(&props.trace_file_text.as_str()).process_all());
 
     let parse_log = {
         let graph_props = graph_props.clone();
-        let trace_file_text = props.trace_file_text.clone();
         let max_line_nr = max_line_nr.value.clone();
         let exclude_theory_inst = *exclude_theory_inst.clone();
         let max_instantiations = max_instantiations.value.clone();
         Callback::from(move |_| {
             let graph_props = graph_props.clone();
-            let trace_file_text = trace_file_text.clone();
-            let parser = Z3Parser::from_str(trace_file_text.as_str());
-            let parser = parser.process_all();
             let InstGraph{inst_graph, ..} = parser.get_instantiation_graph(RenderSettings{
                 max_line_nr,
                 exclude_theory_inst,
@@ -43,9 +40,9 @@ pub fn svg_result(props: &SVGProps) -> Html {
             log::debug!("Finished building dot output");
             wasm_bindgen_futures::spawn_local(
                 async move {
-                   let graphviz = VizInstance::new().await;
-                   let mut options = viz_js::Options::default();
-                //    options.engine = "osage".to_string();
+                    let graphviz = VizInstance::new().await;
+                    let mut options = viz_js::Options::default();
+                    // options.engine = "sfdp".to_string();
                     let svg = graphviz
                         .render_svg_element(dot_output, options)
                         .expect("Could not render graphviz");
@@ -67,7 +64,7 @@ pub fn svg_result(props: &SVGProps) -> Html {
     html! {
         <>
             <div>
-                <h2>{"Specify (optional) render settings"}</h2>
+                <h2>{"Specify (optional) render settings:"}</h2>
                 <UsizeInput label={"Render graph up to line number "} dependency={props.trace_file_text.clone()} input_value={max_line_nr} default_value={usize::MAX} />
                 <ToggleSwitch label={"Ignore theory-solving instantiations: "} dependency={props.trace_file_text.clone()} input_value={exclude_theory_inst} />
                 <UsizeInput label={"Render the n most expensive instantiations where n = "} dependency={props.trace_file_text.clone()} input_value={max_instantiations} default_value={250}/>
