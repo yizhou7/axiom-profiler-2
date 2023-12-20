ce3aab0859f3c87fcc208c344d93bdbf70c81600
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2023-12-20 23:01:49 +0100

    Increase memory allowance

--- smt-log-parser/tests/parse_logs.rs
+++ smt-log-parser/tests/parse_logs.rs
@@ -19,12 +19,14 @@ fn parse_all_logs() {
             println!("Parsing {} ({} MB) with timeout of {timeout:?}", filename.display(), file_size / 1024 / 1024);
             // Some memory usage is still left over from previous loop iterations, so we'll need to subtract that.
             let start_mem = memory_stats::memory_stats().unwrap().physical_mem as u64;
+            // TODO: optimize memory usage and reduce `max_mem`.
+            let max_mem = start_mem + file_size + file_size / 4 + 128*1024*1024;
             let now = Instant::now();
 
             parser.process_check_every(Duration::from_millis(100), |_, _| {
                 assert!(now.elapsed() < timeout, "Parsing took longer than timeout");
-                let physical_mem = (memory_stats::memory_stats().unwrap().physical_mem as u64).saturating_sub(start_mem);
-                assert!(physical_mem < file_size + 512*1024*1024, "Memory usage was {} MB, but file size was {} MB", physical_mem / 1024 / 1024, file_size / 1024 / 1024);
+                let physical_mem = memory_stats::memory_stats().unwrap().physical_mem as u64;
+                assert!(physical_mem < max_mem, "Memory usage was {} MB, but file size was {} MB (max mem {} MB)", physical_mem / 1024 / 1024, file_size / 1024 / 1024, max_mem / 1024 / 1024);
                 true
             });
             drop(parser);
