1b49834c4e8289125e794e8311a2d6e03e1808e7
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2023-12-20 17:32:39 +0100

    Testing

--- smt-log-parser/tests/parse_logs.rs
+++ smt-log-parser/tests/parse_logs.rs
@@ -11,12 +11,13 @@ fn parse_all_logs() {
         let filename = log.unwrap().path();
         let (metadata, parser) = Z3Parser::from_file(&filename).unwrap();
         // Set limit if supported
-        let _ = rlimit::Resource::RSS.set(metadata.len(), rlimit::Resource::RSS.get_hard().unwrap_or(rlimit::INFINITY)).unwrap();
+        let _ = rlimit::Resource::RSS.set(metadata.len() / 1024, rlimit::Resource::RSS.get_hard().unwrap_or(rlimit::INFINITY)).unwrap();
         // Gives 50 millis per MB (or 50 secs per GB)
         // let to = Duration::from_millis(500 + (metadata.len() / 20_000));
         let to = Duration::from_secs(600);
-        println!("Parsing {} with timeout of {to:?}", filename.display());
-        let (timeout, _result) = parser.process_all_timeout(to);
+        println!("Parsing {} ({} MB) with timeout of {to:?}", filename.display(), metadata.len() / 1024 / 1024);
+        let (timeout, result) = parser.process_all_timeout(to);
         assert!(timeout.is_none());
+        drop(result);
     }
 }
