0db0ea5de6b7fb20350cd40ea45dd5d1cbc0c0f1
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2024-01-09 15:44:33 +0100

    Also print overall parse speed

--- smt-log-parser/tests/parse_logs.rs
+++ smt-log-parser/tests/parse_logs.rs
@@ -6,20 +6,22 @@ use smt_log_parser::{LogParser, Z3Parser};
 fn parse_all_logs() {
     std::env::set_var("SLP_TEST_MODE", "true");
 
-    let all_logs = std::fs::read_dir("../logs").unwrap();
+    let mut all_logs: Vec<_> = std::fs::read_dir("../logs").unwrap().map(|r| r.unwrap()).collect();
+    all_logs.sort_by_key(|dir| dir.path());
     for log in all_logs {
         // Put things in a thread to isolate memory usage more than the default.
         let t = std::thread::spawn(move || {
-            let filename = log.unwrap().path();
+            let filename = log.path();
             let (metadata, mut parser) = Z3Parser::from_file(&filename).unwrap();
             let file_size = metadata.len();
+            let file_size_kb = file_size / 1024;
 
             // Gives 100 millis per MB (or 100 secs per GB)
-            let timeout = Duration::from_millis(500 + (file_size / 10_000));
+            let timeout = Duration::from_millis(500 + (file_size_kb / 10));
             println!(
                 "Parsing {} ({} MB) with timeout of {timeout:?}",
                 filename.display(),
-                file_size / 1024 / 1024
+                file_size_kb / 1024
             );
             // Some memory usage is still left over from previous loop iterations, so we'll need to subtract that.
             let start_mem = memory_stats::memory_stats().unwrap().physical_mem as u64;
@@ -39,7 +41,8 @@ fn parse_all_logs() {
                 );
                 true
             });
-            println!("Finished parsing in {:?}", now.elapsed());
+            let elapsed = now.elapsed();
+            println!("Finished parsing in {elapsed:?} ({} kB/ms)", file_size_kb as u128 / elapsed.as_millis());
             println!();
             drop(parser);
         });
